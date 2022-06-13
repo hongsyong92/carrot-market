fa41b715be75ed8d7f97a00b5725372f9fa893e3
SYONG <ghdgus24@naver.com>
2022-06-13 18:01:21 +0900

    tailwind setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-5 rounded-2xl shadow-2xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-3">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$80</span>
+        </div>
+        <div className="flex justify-between mt-2 py-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span className="font-semibold">$99</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          Checkout
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+    </div>
+  );
 };
 
 export default Home;
